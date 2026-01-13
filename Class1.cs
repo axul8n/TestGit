79e5b61a3b5a4765bdd216056f01ad7374052724
axul8n <gregoric.lukas@outlook.com>
2026-01-13 10:27:17 +0100

    Fourth

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,10 @@
         public void Main(string[] args)
         {
             Console.WriteLine("I LOVE C#!!");
-
+            Console.WriteLine("Na kmetiji je lepo");
+            Console.WriteLine("I-A-I-A-O");
+            Console.WriteLine("Tam imamo osla dva");
+            Console.WriteLine("I-A-I-A-O");
         }
     }
 
