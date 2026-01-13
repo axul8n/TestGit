eb48331212e92c38bfbd67e309ad53fc92829247
axul8n <gregoric.lukas@outlook.com>
2026-01-13 10:51:47 +0100

    Push 5

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public void Main(string[] args)
+    {
+        Console.WriteLine("I LOVE C#!!");
+        Console.WriteLine("Na kmetiji je lepo");
+        Console.WriteLine("I-A-I-A-O");
+        Console.WriteLine("Tam imamo osla dva");
+        Console.WriteLine("I-A-I-A-O");
+    }
+}
+
---
+++ Test1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Test1;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("I LOVE C#!!");
+        Console.WriteLine("Na kmetiji je lepo");
+        Console.WriteLine("I-A-I-A-O");
+        Console.WriteLine("Tam imamo osla dva");
+        PrintMessage("I-A-I-A-O");
+
+        Sample();
+    }
+  
+    public static void PrintMessage(String a)
+    {
+        Console.WriteLine(a);
+    }   
+
+    public static void Sample() 
+    {
+    
+        Console.WriteLine("This is a sample method.");
+        Console.WriteLine("It does not do much, but it is here for demonstration.");
+
+    }
+
+
+
+}
+
+
+
+
+
