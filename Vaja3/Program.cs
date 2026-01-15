5d02e9975cafed41cecd6de2a310f7167d59fb5c
axul8n <gregoric.lukas@outlook.com>
2026-01-15 08:14:40 +0100

    upd 2

--- Vaja3/Program.cs
+++ Vaja3/Program.cs
@@ -5,13 +5,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
             
+        int a = 5;
+        int b = 10;
 
 
-
-
-
+        Console.WriteLine("a+b: "+(a+b)+" a: "+a+" b: "+b);
     }
 }
 
