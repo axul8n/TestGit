767ba0a39f52e53fbce90d02bfc33b15d3a3b694
axul8n <gregoric.lukas@outlook.com>
2026-01-15 08:12:00 +0100

    Up 1

---
+++ Vaja3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Vaja3;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+            
+
+
+
+
+
+    }
+}
+
+
